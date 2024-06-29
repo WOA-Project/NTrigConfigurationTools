4a3236ef785541cfea6b0de6277f8811bd62aa0f
Gustave Monce <gustave.monce@outlook.com>
2024-06-29 18:06:53 +0200

    Update PSCFGData_libsurfacetouch.cs

--- PSCFGDataReader/PSCFGData_libsurfacetouch.cs
+++ PSCFGDataReader/PSCFGData_libsurfacetouch.cs
@@ -1266,18 +1266,24 @@ namespace PSCFGDataReader
 
         public struct PenCenterOfMassCfg
         {
-            public uint ConstSpecificCfgPenVendorSize { get; set; }
-            [field: MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
-            public PenVendorSpecificCfgType[] PenVendorSpecificCfg { get; set; }
+            public uint MEMInitialValue { get; set; }
             public float MEMInkHoverLowThrHysteresisFactor { get; set; }
             public float MEMInkHoverHighThrHysteresisFactor { get; set; }
+            public uint PenEnergyInitialValue { get; set; }
             public float PenEnergyAlpha { get; set; }
+            public float Unknown0 { get; set; }
             public float TiltPoly1 { get; set; }
             public float TiltPoly2 { get; set; }
             public float TiltPoly3 { get; set; }
+            [field: MarshalAs(UnmanagedType.ByValArray, SizeConst = 12)]
+            public byte[] Unknown1 { get; set; } // 0x24
+            [field: MarshalAs(UnmanagedType.ByValArray, SizeConst = 3)]
+            public float[] OffsetPoly { get; set; }
             public float EdgeRingOffsetForTA { get; set; }
             public float EdgeTipOffsetForTA { get; set; }
             public float MinTiltValueForAzimuthBasedCorrection { get; set; }
+            [field: MarshalAs(UnmanagedType.ByValArray, SizeConst = 2)]
+            public float[] AzimuthBasedCorrectionFactor { get; set; }
             public float OffsetCorrectionAzimuthIIRCoef { get; set; }
             public float OffsetCorrectionTiltIIRCoef { get; set; }
             public byte RunOffsetCalculation { get; set; }
@@ -1286,10 +1292,23 @@ namespace PSCFGDataReader
             public uint ConstForEdgeCoMBorderSize { get; set; }
             [field: MarshalAs(UnmanagedType.ByValArray, SizeConst = 8)]
             public PenCoMOnEdgesCoefficientsSetPerBorder[]  PenCenterOfMassOnEdgesCoefficientsSetPerBorder { get; set; }
+            public uint ConstSpecificCfgPenVendorSize { get; set; }
+            public uint ConstTipEdgeCoMArrayCoefficientsSize { get; set; }
+            public uint ConstTipEdgeCoMArrayFeatureSize { get; set; }
+            [field: MarshalAs(UnmanagedType.ByValArray, SizeConst = 12)]
+            public float[] TipEdgeCoMArray { get; set; }
+            [field: MarshalAs(UnmanagedType.ByValArray, SizeConst = 12)]
+            public float[] TipEdgeCoMArray2 { get; set; }
+            [field: MarshalAs(UnmanagedType.ByValArray, SizeConst = 12)]
+            public float[] TipEdgeCoMArray3 { get; set; }
             public uint TailEdgeCoMArrayCoefficientsSize { get; set; }
             public uint TailEdgeCoMArrayFeatureSize { get; set; }
             [field: MarshalAs(UnmanagedType.ByValArray, SizeConst = 12)]
             public float[] TailEdgeCoMArray { get; set; }
+            public uint ConstCentroidArrayNumOfFreqTypes { get; set; }
+            public uint ConstCentroidArrayNumOfAxes { get; set; }
+            [field: MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
+            public float[] CentroidArray { get; set; }
             public uint LowFreqTipOnlyVAxisArraySize { get; set; }
             [field: MarshalAs(UnmanagedType.ByValArray, SizeConst = 6)]
             public float[] LowFreqTipOnlyVAxisArray { get; set; }
@@ -1320,49 +1339,98 @@ namespace PSCFGDataReader
             public uint TiltKeyphusArraySize { get; set; }
             [field: MarshalAs(UnmanagedType.ByValArray, SizeConst = 33)]
             public float[] TiltKeyphusArray { get; set; }
+            public uint PucHeuristicThr5FreqSize { get; set; }
+            public uint PucHeuristicThr5Size { get; set; }
+            [field: MarshalAs(UnmanagedType.ByValArray, SizeConst = 6)]
+            public ushort[] PucHeuristicThr5 { get; set; }
+            public uint PucHeuristicVal5FreqSize { get; set; }
+            public uint PucHeuristicVal5Size { get; set; }
+            [field: MarshalAs(UnmanagedType.ByValArray, SizeConst = 6)]
+            public ushort[] PucHeuristicVal5 { get; set; }
+            public uint PucHeuristicFirstThr3FreqSize { get; set; }
+            public uint PucHeuristicFirstThr3Size { get; set; }
+            [field: MarshalAs(UnmanagedType.ByValArray, SizeConst = 6)]
+            public ushort[] PucHeuristicFirstThr3 { get; set; }
+            public uint PucHeuristicFirstThr1_2FreqSize { get; set; }
+            public uint PucHeuristicFirstThr1_2Size { get; set; }
+            [field: MarshalAs(UnmanagedType.ByValArray, SizeConst = 6)]
+            public ushort[] PucHeuristicFirstThr1_2 { get; set; }
             public uint PucHeuristicFirstVal3FreqSize { get; set; }
             public uint PucHeuristicFirstVal3Size { get; set; }
             [field: MarshalAs(UnmanagedType.ByValArray, SizeConst = 6)]
             public ushort[] PucHeuristicFirstVal3 { get; set; }
+            public uint PucHeuristicSecThr3FreqSize { get; set; }
+            public uint PucHeuristicSecThr3Size { get; set; }
+            [field: MarshalAs(UnmanagedType.ByValArray, SizeConst = 6)]
+            public ushort[] PucHeuristicSecThr3 { get; set; }
+            public uint PucHeuristicSecThr1_2FreqSize { get; set; }
+            public uint PucHeuristicSecThr1_2Size { get; set; }
+            [field: MarshalAs(UnmanagedType.ByValArray, SizeConst = 6)]
+            public ushort[] PucHeuristicSecThr1_2 { get; set; }
             public uint PucHeuristicSecVal3FreqSize { get; set; }
             public uint PucHeuristicSecVal3Size { get; set; }
             [field: MarshalAs(UnmanagedType.ByValArray, SizeConst = 6)]
             public ushort[] PucHeuristicSecVal3 { get; set; }
+            public uint PucHeuristicThirdThr3FreqSize { get; set; }
+            public uint PucHeuristicThirdThr3Size { get; set; }
+            [field: MarshalAs(UnmanagedType.ByValArray, SizeConst = 6)]
+            public ushort[] PucHeuristicThirdThr3 { get; set; }
             public uint PucHeuristicThirdVal3FreqSize { get; set; }
             public uint PucHeuristicThirdVal3Size { get; set; }
             [field: MarshalAs(UnmanagedType.ByValArray, SizeConst = 6)]
             public ushort[] PucHeuristicThirdVal3 { get; set; }
+            public uint PucHeuristicDeltaThr3_5FreqSize { get; set; }
+            public uint PucHeuristicDeltaThr3_5Size { get; set; }
+            [field: MarshalAs(UnmanagedType.ByValArray, SizeConst = 6)]
+            public ushort[] PucHeuristicDeltaThr3_5 { get; set; }
             public uint PucHeuristicAddtive3_5FreqSize { get; set; }
             public uint PucHeuristicAddtive3_5Size { get; set; }
             [field: MarshalAs(UnmanagedType.ByValArray, SizeConst = 6)]
             public ushort[] PucHeuristicAddtive3_5 { get; set; }
+            public uint PucHeuristicDeltaThr2_3FreqSize { get; set; }
+            public uint PucHeuristicDeltaThr2_3Size { get; set; }
+            [field: MarshalAs(UnmanagedType.ByValArray, SizeConst = 6)]
+            public ushort[] PucHeuristicDeltaThr2_3 { get; set; }
+            public uint PucHeuristicFirstThr4FreqSize { get; set; }
+            public uint PucHeuristicFirstThr4Size { get; set; }
+            [field: MarshalAs(UnmanagedType.ByValArray, SizeConst = 6)]
+            public ushort[] PucHeuristicFirstThr4 { get; set; }
             public uint PucHeuristicFirstVal4FreqSize { get; set; }
             public uint PucHeuristicFirstVal4Size { get; set; }
             [field: MarshalAs(UnmanagedType.ByValArray, SizeConst = 6)]
             public ushort[] PucHeuristicFirstVal4 { get; set; }
+            public uint PucHeuristicSecThr4FreqSize { get; set; }
+            public uint PucHeuristicSecThr4Size { get; set; }
+            [field: MarshalAs(UnmanagedType.ByValArray, SizeConst = 6)]
+            public ushort[] PucHeuristicSecThr4 { get; set; }
             public uint PucHeuristicSecVal4FreqSize { get; set; }
             public uint PucHeuristicSecVal4Size { get; set; }
             [field: MarshalAs(UnmanagedType.ByValArray, SizeConst = 6)]
             public ushort[] PucHeuristicSecVal4 { get; set; }
+            public uint PucHeuristicThr2FreqSize { get; set; }
+            public uint PucHeuristicThr2Size { get; set; }
+            [field: MarshalAs(UnmanagedType.ByValArray, SizeConst = 6)]
+            public ushort[] PucHeuristicThr2 { get; set; }
             public uint PucHeuristicVal2FreqSize { get; set; }
             public uint PucHeuristicVal2Size { get; set; }
             [field: MarshalAs(UnmanagedType.ByValArray, SizeConst = 6)]
             public ushort[] PucHeuristicVal2 { get; set; }
+            public uint PucHeuristicDeltaThr2_4FreqSize { get; set; }
+            public uint PucHeuristicDeltaThr2_4Size { get; set; }
+            [field: MarshalAs(UnmanagedType.ByValArray, SizeConst = 6)]
+            public ushort[] PucHeuristicDeltaThr2_4 { get; set; }
             public uint PucHeuristicAddtive2_4FreqSize { get; set; }
             public uint PucHeuristicAddtive2_4Size { get; set; }
             [field: MarshalAs(UnmanagedType.ByValArray, SizeConst = 6)]
             public ushort[] PucHeuristicAddtive2_4 { get; set; }
             public uint PucHeuristicMaxValidSignalValForS4orS5 { get; set; }
-            public uint RtiaValuePerPlatformSize { get; set; }
-            public uint RtiaValueSize { get; set; }
-            [field: MarshalAs(UnmanagedType.ByValArray, SizeConst = 63)]
-            public uint[] RtiaValue { get; set; }
-            public uint RtiaPerPlatformSize { get; set; }
-            public uint RtiaValuePerGainSize { get; set; }
-            [field: MarshalAs(UnmanagedType.ByValArray, SizeConst = 9)]
-            public byte[] RtiaValuePerGain { get; set; }
+            [field: MarshalAs(UnmanagedType.ByValArray, SizeConst = 16)]
+            public ushort[] RegNibble2RtiaValue { get; set; }
+            [field: MarshalAs(UnmanagedType.ByValArray, SizeConst = 6)]
+            public ushort[] BlackPrintSqrThreshold { get; set; }
             public uint PreLsPhaseCriteriaAmplitudeThresholdSqr { get; set; }
             public float PreLsPhaseCriteriaAngleThreshold { get; set; }
+            public float Unknown2 { get; set; }
             public RingValidationCfg RingValidation { get; set; }
             public byte MaxNumOfBordersCoefficients { get; set; }
             public byte IsPucHeuristicEnabled { get; set; }
@@ -1372,7 +1440,6 @@ namespace PSCFGDataReader
             public uint PenEdgesThresholdsPosCount { get; set; }
             [field: MarshalAs(UnmanagedType.ByValArray, SizeConst = 8)]
             public float[] PenEdgesThresholds { get; set; }
-            public float BorderZfiPenVoltageRatio { get; set; }
         };
 
         public struct PenDigitalProcessingCfg
